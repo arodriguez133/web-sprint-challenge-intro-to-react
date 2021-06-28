be7b53847413516997ec4bd465c3731f9a968f3f
arodriguez133 <arodriguez133@icloud.com>
2021-06-27 20:31:14 -0500

    Finished Project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from 'react';
+
+import React, {useState, useEffect} from 'react';
+import CharacterDetails from './components/Character';
+import Axios from 'axios';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  useEffect(() => {
+    Axios.get("https://swapi.dev/api/people/")
+    .then(response => {
+      setCharacters(response.data.results);
+    })
+    .catch(error => {console.log('Server Error', error)
+  });
+}, []);
+
+console.log("char", characters);
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">React Wars</h1>
+      <ul>
+        {characters.map(character => {
+          return (
+          <CharacterDetails 
+          name= {character.name} 
+          height = {character.height} 
+          mass= {character.mass}/>
+        )})}
+      </ul>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+  
+import React from 'react';
+import Styled from 'styled-components';
+
+const IdCard = Styled.div `
+border: 1px solid black;
+margin: 10px auto;
+width: 50%;
+`
+
+function CharacterDetails(props){
+ console.log("props from app", props)
+    return (
+        <IdCard>
+            <h1>Name: {props.name}</h1>
+            <h3>Height: {props.height}</h3>
+            <h3>Mass: {props.mass}</h3>
+            
+        </IdCard>
+    )
+}
+
+export default CharacterDetails;
